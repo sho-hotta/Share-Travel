337957a8f53a06be612403078133476ae33fb568
sho-hotta <sho1127.db@gmail.com>
2021-06-22 18:20:41 +0900

    [Add] root_urlを指定 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'plans/index'
-  get 'plans/show'
-  get 'plans/new'
-  get 'plans/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'plans#index'
+  resources :plans
 end
