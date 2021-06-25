abe0f0f0031fa74678b9ffb68062cb63d50f3647
sho-hotta <sho1127.db@gmail.com>
2021-06-25 11:03:53 +0900

    [Add] Planモデルにバリデーションを追加 #7

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,8 @@
 class Plan < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :region, presence: true
+  validates :prefecture, presence: true
 end
