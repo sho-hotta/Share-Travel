6f6cc69b77f00bfb556fa887543230e77c20fb02
sho-hotta <sho1127.db@gmail.com>
2021-06-25 16:35:46 +0900

    [Add] Destinationモデルにバリデーションを追加 #7

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ApplicationRecord
   belongs_to :plan
+
+  validates :plan_id, presence: true
+  validates :name, presence: true
+  validates :description, length: { maximum: 300 }
 end
