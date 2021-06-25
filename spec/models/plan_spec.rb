ec42b5e2138a4c2ffa6184c167b6dba29060e7db
sho-hotta <sho1127.db@gmail.com>
2021-06-25 11:32:39 +0900

    [Add] PlanモデルのModel Specを追加 #7

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @plan = create(:plan)
+    @user = @plan.user
+  end
+
+  context 'バリデーション' do
+    it 'user_idが存在しなければ無効' do
+      @plan.user_id = nil
+      expect(@plan).to be_invalid
+    end
+
+    it 'user_idが存在すれば有効' do
+      expect(@plan).to be_valid
+    end
+
+    it 'titleが存在しなければ無効' do
+      @plan.title = nil
+      expect(@plan).to be_invalid
+    end
+
+    it 'titleが31文字以上であれば無効' do
+      @plan.title = 'a' * 31
+      expect(@plan).to be_invalid
+    end
+
+    it 'titleが30文字であれば有効' do
+      @plan.title = 'a' * 30
+      expect(@plan).to be_valid
+    end
+
+    it 'regionが存在しなければ無効' do
+      @plan.region = nil
+      expect(@plan).to be_invalid
+    end
+
+    it 'regionが存在すれば有効' do
+      expect(@plan).to be_valid
+    end
+
+    it 'prefectureが存在しなければ無効' do
+      @plan.prefecture = nil
+      expect(@plan).to be_invalid
+    end
+
+    it 'prefectureが存在すれば有効' do
+      expect(@plan).to be_valid
+    end
+  end
+
+  context 'dependent: :destroy' do
+    it '機能する' do
+      expect do
+        @user.destroy
+      end.to change { Plan.count }.by(-1)
+    end
+  end
 end
