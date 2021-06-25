b302cb31aeda30d12aecf92c75c7cc027f95ef57
sho-hotta <sho1127.db@gmail.com>
2021-06-25 10:53:10 +0900

    [Add] plansテーブルを追加 #7

--- db/migrate/20210625010727_create_plans.rb
+++ db/migrate/20210625010727_create_plans.rb
@@ -1,10 +1,24 @@
 class CreatePlans < ActiveRecord::Migration[6.1]
   def change
     create_table :plans do |t|
-      t.string :title
+      t.string :title, null: false, limit: 30
+      t.column :region, "ENUM('北海道・東北', '北関東', '首都圏', '甲信越', '東海',
+                              '北陸', '近畿', '山陽・山陰', '四国', '九州・沖縄') DEFAULT '北海道・東北' NOT NULL"
+      t.column :prefecture, "ENUM('北海道', '青森', '岩手', '宮城', '秋田', '山形', '福島',
+                                  '栃木', '群馬', '茨城',
+                                  '埼玉', '千葉', '東京', '神奈川',
+                                  '山梨', '長野', '新潟',
+                                  '静岡', '岐阜', '愛知', '三重',
+                                  '富山', '石川', '福井',
+                                  '滋賀', '京都', '大阪', '兵庫', '奈良', '和歌山',
+                                  '鳥取', '島根', '岡山', '広島', '山口',
+                                  '徳島', '香川', '愛媛', '高知',
+                                  '福岡', '佐賀', '長崎', '熊本', '大分', '宮崎', '鹿児島', '沖縄') DEFAULT '北海道' NOT NULL"
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :plans, %i[user_id created_at]
+    add_index :plans, %i[user_id updated_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_093001) do
+ActiveRecord::Schema.define(version: 2021_06_25_010727) do
+
+  create_table "plans", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", limit: 30, null: false
+    t.column "region", "enum('北海道・東北','北関東','首都圏','甲信越','東海','北陸','近畿','山陽・山陰','四国','九州・沖縄')", default: "北海道・東北", null: false
+    t.column "prefecture", "enum('北海道','青森','岩手','宮城','秋田','山形','福島','栃木','群馬','茨城','埼玉','千葉','東京','神奈川','山梨','長野','新潟','静岡','岐阜','愛知','三重','富山','石川','福井','滋賀','京都','大阪','兵庫','奈良','和歌山','鳥取','島根','岡山','広島','山口','徳島','香川','愛媛','高知','福岡','佐賀','長崎','熊本','大分','宮崎','鹿児島','沖縄')", default: "北海道", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_plans_on_user_id_and_created_at"
+    t.index ["user_id", "updated_at"], name: "index_plans_on_user_id_and_updated_at"
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -37,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_06_22_093001) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plans", "users"
 end
