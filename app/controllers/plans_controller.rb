fc7c3d2edd284d563610d08f42b641901a59df45
sho-hotta <sho1127.db@gmail.com>
2021-06-27 12:58:39 +0900

    [Add] プラン編集機能を追加 #9

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,6 @@
 class PlansController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create edit update]
+  before_action :correct_user, only: %i[edit update]
 
   def index; end
 
@@ -24,9 +25,24 @@ class PlansController < ApplicationController
     @plan = Plan.find(params[:id])
   end
 
+  def update
+    @plan = Plan.find(params[:id])
+    if @plan.update(plan_params)
+      flash[:success] = 'プランを編集しました！'
+      redirect_to @plan
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def plan_params
     params.require(:plan).permit(:title, :region, :prefecture, destinations_attributes: %i[name description _destroy id])
+  end
+
+  def correct_user
+    @user = Plan.find(params[:id]).user
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
