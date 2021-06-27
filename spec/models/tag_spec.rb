dfffaefa2a90d715715fab53ce793ae5f8a9df8c
sho-hotta <sho1127.db@gmail.com>
2021-06-27 23:32:28 +0900

    [Add] TagモデルのModel Specを追加 #12

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = create(:tag)
+  end
+
+  context 'バリデーション' do
+    it 'wordが存在しなければ無効' do
+      @tag.word = nil
+      expect(@tag).to be_invalid
+    end
+
+    it 'wordは一意である' do
+      another_tag = @tag.dup
+      expect(another_tag).to be_invalid
+    end
+
+    it 'wordが11文字以上であれば無効' do
+      @tag.word = 'a' * 11
+      expect(@tag).to be_invalid
+    end
+
+    it 'wordが10文字であれば有効' do
+      @tag.word = 'a' * 10
+      expect(@tag).to be_valid
+    end
+  end
 end
