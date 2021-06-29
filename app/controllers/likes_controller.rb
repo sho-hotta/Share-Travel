b268809873e116b84b72555d44ac526d8c9e6ecc
sho-hotta <sho1127.db@gmail.com>
2021-06-29 18:17:01 +0900

    [Add] いいね機能を追加 #16

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+
+  def create
+    @like_plan = current_user.likes.build(plan_id: params[:plan_id])
+    @like_plan.save
+  end
+
+  def destroy
+    @unlike_plan = Like.find_by(plan_id: params[:plan_id], user_id: current_user.id).plan
+    @unlike_plan.destroy
+  end
 end
