fe935d84ad925b4ccc73238c61d66695cf3c864f
sho-hotta <sho1127.db@gmail.com>
2021-06-29 17:17:54 +0900

    [Add] LikeモデルのModel Specを追加 #16

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :like do
-    user { nil }
-    plan { nil }
+    plan
+    user { plan.user }
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'バリデーション' do
+    before do
+      @like = create(:like)
+    end
+
+    it 'user_idが存在しなければ無効' do
+      @like.user_id = nil
+      expect(@like).to be_invalid
+    end
+
+    it 'plan_idが存在しなければ無効' do
+      @like.plan_id = nil
+      expect(@like).to be_invalid
+    end
+
+    it 'user_idとplan_idの組み合わせは一意である' do
+      another_like = @like.dup
+      expect(another_like).to be_invalid
+    end
+  end
 end
