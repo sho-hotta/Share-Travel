8fbe6dec7bfa1504b010b2c8ac78b00b4e0a5aa2
sho-hotta <sho1127.db@gmail.com>
2021-06-29 17:30:37 +0900

    [Add] User/Like/Planモデルを関連付け #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :plans, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_plans, through: :likes, source: :plan
 
   validates :name, presence: true, length: { maximum: 20 }
 end
