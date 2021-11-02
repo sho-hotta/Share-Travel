96ef000f13546f2c43804c71c91a640c80dbc112
sho-hotta <sho1127.db@gmail.com>
2021-11-02 14:13:20 +0900

    [Add] ゲストログイン機能を追加 #24

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
   def like?(plan)
     self.like_plans.include?(plan)
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'ゲスト'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
