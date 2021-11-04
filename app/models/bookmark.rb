6e35ec713e810eeab778006bf0ec05cba5dc09dd
sho-hotta <sho1127.db@gmail.com>
2021-11-04 15:55:49 +0900

    [Add] Bookmarkモデルにバリデーションを追加 #17

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :plan
+
+  validates :user_id, presence: true
+  validates :plan_id, presence: true
+  validates :user_id, uniqueness: { scope: :plan_id }
 end
